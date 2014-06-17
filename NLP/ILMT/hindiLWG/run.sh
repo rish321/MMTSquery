ce889925a24a51d8ba01beb3bba3a70cabd589e0
pramesh94 <pramesh@Pramesh-PC>
2014-06-18 02:58:13 +0530

    removed while loop from run.sh

--- NLP/ILMT/hindiLWG/run.sh
+++ NLP/ILMT/hindiLWG/run.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 VAANEE_DIR=$3/ILMT/
-
-
-while read line
-do
 	
-	echo "$line" > $2/.tmp/tempin.txt
-	shallow_parser_hin $2/.tmp/tempin.txt $2/.tmp/tempout.txt
-	cat $PWD/OUTPUT$2/.tmp/postagger$2/.tmp > $2/.tmp/corpus.train.pos.hn
-	perl $VAANEE_DIR/hindiLWG/ssf2lwg.pl < $2/.tmp/corpus.train.pos.hn > $2/.tmp/corpus.train.lwgI.hn
-	perl $VAANEE_DIR/hindiLWG/hnd_lwg.pl  $2/.tmp/corpus.train.lwgI.hn > $2/.tmp/corpus.train.lwgI$2/.tmp.hn
-	perl $VAANEE_DIR/hindiLWG/hindi_feature.pl $2/.tmp/corpus.train.lwgI$2/.tmp.hn >> $2/.tmp/temporaryfile1
-
-done < $1
+echo "$line" > $2/.tmp/tempin.txt
+shallow_parser_hin $1 $2/.tmp/tempout.txt
+cat $PWD/OUTPUT$2/.tmp/postagger$2/.tmp > $2/.tmp/corpus.train.pos.hn
+perl $VAANEE_DIR/hindiLWG/ssf2lwg.pl < $2/.tmp/corpus.train.pos.hn > $2/.tmp/corpus.train.lwgI.hn
+perl $VAANEE_DIR/hindiLWG/hnd_lwg.pl  $2/.tmp/corpus.train.lwgI.hn > $2/.tmp/corpus.train.lwgI.tmp.hn
+perl $VAANEE_DIR/hindiLWG/hindi_feature.pl $2/.tmp/corpus.train.lwgI.tmp.hn > $2/.tmp/temp
 
 path=$HOME/sampark/shallow_parser_hin/bin/sys/convertor/hin
 
-cat $2/.tmp/temporaryfile1 > $2/.tmp/temp
 sed -i 's/^$/::/g' $2/.tmp/temp
 sed -i 's/\t/\n/g' $2/.tmp/temp
 tr '\n' ' ' < $2/.tmp/temp > $2/.tmp/temp2
@@ -30,5 +23,3 @@ sed -i 's/jAwI_hE/jA hE/g' $2/.tmp/temp2
 sed -i 's/AwI_hE/A hE/g' $2/.tmp/temp2
 $path/bin/hin/wx_to_i8.exe <  $2/.tmp/temp2 > $2/.tmp/temp3
 perl $path/bin/hin/iscii2utf8.pl < $2/.tmp/temp3
-
-rm $2/.tmp/temporaryfile1
